090ed1e9139422a8e860939f4519ebfb90ca1c41
hdy <d-yeong@naver.com>
2022-05-19 21:21:19 +0900

    이벤트리스트

---
+++ team_project3/src/main/java/svc/EventListService.java
@@ -0,0 +1,49 @@
+package svc;
+
+import static db.JdbcUtil.*;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import dao.AdminDAO;
+import vo.EventBean;
+
+public class EventListService {
+
+	public int getListCount(String tableName) {
+		System.out.println("EventListService - getListCount()");
+		
+		int listCount = 0;
+		
+		Connection con = getConnection();
+		AdminDAO adminDAO= AdminDAO.getInstance();
+		
+		adminDAO.setConnection(con);
+		
+		listCount = adminDAO.selectListCount(tableName);
+		
+		close(con);
+		
+		return listCount;
+		
+	}
+
+	public ArrayList<EventBean> getEventleList(int pageNum, int listLimit) {
+		System.out.println("NoticeListService - getNoticeleList()");
+		
+		ArrayList<EventBean> eventList = null;
+		
+		Connection con = getConnection(); 
+		AdminDAO adminDAO= AdminDAO.getInstance();
+		
+		adminDAO.setConnection(con);
+		
+		eventList = adminDAO.selectEventList(pageNum, listLimit);
+		
+		close(con);
+		
+		return eventList;
+	}
+	
+
+}
