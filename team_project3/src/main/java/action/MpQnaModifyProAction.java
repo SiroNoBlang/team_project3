7da76d4d870e54101a467e7cf26b9a282526269a
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2022-05-23 12:25:29 +0900

    메인페이지 qna 수정 오류 해결

--- team_project3/src/main/java/action/MpQnaModifyProAction.java
+++ team_project3/src/main/java/action/MpQnaModifyProAction.java
@@ -1,16 +1,54 @@
 package action;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import svc.QnaModifyProService;
 import vo.ActionForward;
+import vo.QnaBean;
 
 public class MpQnaModifyProAction implements Action {
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+//		System.out.println("MpQnaModifyProAction");
+		ActionForward forward = null;
+		
+		int qna_num = Integer.parseInt(request.getParameter("qna_num"));
+		String qna_title = request.getParameter("qna_title");
+		String qna_nickname = request.getParameter("qna_nickname");
+		String qna_date = request.getParameter("qna_date");
+		String qna_content = request.getParameter("qna_content");
+		
+			
+		QnaBean qna = new QnaBean();
+		qna.setQna_num(qna_num);
+		qna.setQna_title(qna_title);
+		qna.setQna_nickname(qna_nickname);
+		qna.setQna_write_date(qna_date);
+		qna.setQna_content(qna_content);
+		
+//		System.out.println(qna);
+		
+		QnaModifyProService service = new QnaModifyProService();
+		boolean isQnaModifySuccess = service.qnaModifyArticle(qna);
+		
+		if(!isQnaModifySuccess) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('수정 실패!')");
+			out.println("history.back()");
+			out.println("</script>");
+		} else {
+			forward = new ActionForward();
+			forward.setPath("CommunityQna.ma?page=" + request.getParameter("page"));
+			forward.setRedirect(true);
+		}
+		
+		return forward;
 	}
 
 }
